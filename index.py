65bdd3fc4bf7ab164bac5f4145bb56fb242fa1c2
Ikram Irzayev <ikram.irzayev13@gmail.com>
2026-04-18 22:02:00 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,19 @@
+import requests
+import time
+import random
+
+# Lambda'dan aldığın URL'yi buraya yapıştır
+URL = "https://kmhj56l57bvd46jz24wsufntf40qjzru.lambda-url.eu-north-1.on.aws/"
+
+while True:
+    veri = {
+        "vehicleId": "Kamyon-34",
+        "hiz": random.randint(60, 110), # Rastgele hız
+        "yakit": random.randint(10, 100),
+        "timestamp": int(time.time())
+    }
+    
+    response = requests.post(URL, json=veri)
+    print(f"Veri gitti: {veri} - Sunucu Yanıtı: {response.text}")
+    
+    time.sleep(5) # 5 saniyede bir veri gönder
